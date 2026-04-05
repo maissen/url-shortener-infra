f8d3562c299ed2663b81030bb5ede39e9b6e287c
Maissen <maissen.developer500@gmail.com>
2026-04-05 19:58:31 +0100

    feat: extend VPC networking config

--- main.tf
+++ main.tf
@@ -47,20 +47,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Private Subnets
-resource "aws_subnet" "private" {
-  count = 2
-
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index + 2}.0/24"
-  availability_zone       = data.aws_availability_zones.azs.names[count.index]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-${count.index}"
-  }
-}
-
 # Public route table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -84,4 +70,60 @@ resource "aws_route_table_association" "public_assoc" {
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index + 2}.0/24"
+  availability_zone       = data.aws_availability_zones.azs.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-${count.index}"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Route to Internet
+resource "aws_route" "private_internet" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_gw.id
+}
+
+
+# Associate private subnets with private rt
+resource "aws_route_table_association" "private_assoc" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
