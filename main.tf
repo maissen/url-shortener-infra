f287dc8a6abcfb2a9f8c4d52c113ebc08ad7d8c6
Maissen <maissen.developer500@gmail.com>
2026-04-05 19:43:01 +0100

    style: run terraform fmt command

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 # VPC
