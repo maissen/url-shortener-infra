23dfee9f3748c92450fd9f16517f78eca65760ab
Maissen <maissen.developer500@gmail.com>
2026-04-05 19:42:12 +0100

    feat: extend VPC config

--- main.tf
+++ main.tf
@@ -60,3 +60,28 @@ resource "aws_subnet" "private" {
     Name = "public-${count.index}"
   }
 }
+
+# Public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Route to Internet
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+
+# Associate public subnets with public rt
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
