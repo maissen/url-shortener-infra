2ec75b4136ba8f4ffad3ff821a5f3f94acaab6be
Maissen <maissen.developer500@gmail.com>
2026-04-07 10:57:27 +0100

    feat: create configs for environments/prod

---
+++ environments/prod/backend.hcl
@@ -0,0 +1,5 @@
+bucket         = "maiss-terraform-state-bucket2"
+key            = "prod/terraform.tfstate"
+region         = "us-east-2"
+dynamodb_table = "terraform-locks"
+encrypt        = true
---
+++ environments/prod/main.tf
@@ -0,0 +1,34 @@
+module "networking" {
+  source = "../../modules/networking"
+
+  name_prefix          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  azs                  = var.azs
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  name_prefix     = var.environment
+  repository_name = var.ecr_repo_name
+}
+
+module "compute" {
+  source = "../../modules/compute"
+
+  name_prefix = var.environment
+
+  log_region = var.region
+  vpc_id              = module.networking.vpc_id
+  public_subnet_ids   = module.networking.public_subnet_ids
+  private_subnet_ids  = module.networking.private_subnet_ids
+
+  container_name = var.container_name
+  container_port = var.container_port
+
+  image = "${module.ecr.repository_url}:${var.image_tag}"
+  desired_count  = var.desired_count
+  enable_deletion_protection = var.enable_alb_deletion_protection
+}
