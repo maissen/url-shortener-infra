f305272fe6b4a23682ca40d1174b9ec3a2125489
Maissen <maissen.developer500@gmail.com>
2026-04-08 10:42:37 +0100

    fix: update AWS Github role trust policy

--- bootstrap/github-oidc/main.tf
+++ bootstrap/github-oidc/main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
         }
       }
     }]
