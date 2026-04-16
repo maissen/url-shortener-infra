e3ec5af6e7313cb8a1c78b83de319daafc452320
Maissen <maissen.developer500@gmail.com>
2026-04-16 11:30:51 +0100

    chore: delete unused 'ECR' module from modules/

--- modules/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  description = "ECR repository URL (used to push/pull images)"
-  value       = aws_ecr_repository.ecr.repository_url
-}
-
-output "repository_arn" {
-  description = "ECR repository ARN"
-  value       = aws_ecr_repository.ecr.arn
-}
