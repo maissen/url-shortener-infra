713f4901ff5f0784c652d944eab0eb1a40a7e74e
Maissen <maissen.developer500@gmail.com>
2026-04-16 18:10:21 +0100

    feat: add a new 'alb_arn' output in compute module

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -3,6 +3,11 @@ output "alb_dns_name" {
   value       = aws_lb.alb.dns_name
 }
 
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.alb.arn
+}
+
 output "alb_zone_id" {
   description = "Zone ID of the ALB"
   value       = aws_lb.alb.zone_id
